2d6885bc394aa49f222a6be0545cc9cf8f496ea7
Carlos Mulero Haro <carlosmh2000@gmail.com>
2024-11-08 21:17:37 +0100

    Cuadrar estilo formulario unir color

--- src/app/interfaz-tutor/preguntas-unir-color/preguntas-unir-color.page.ts
+++ src/app/interfaz-tutor/preguntas-unir-color/preguntas-unir-color.page.ts
@@ -4,7 +4,7 @@ import { ActionSheetController, ModalController, NavController } from '@ionic/an
 import { PreguntaUnir } from 'src/app/models/pregunta-unir.model';
 import { AudioService } from 'src/app/services/audio.service';
 import { CamaraService } from 'src/app/services/camara.service';
-import {openGaleriaModal} from "../../utils";
+import { openGaleriaModal } from "../../utils";
 
 @Component({
   selector: 'app-preguntas-unir-color',
@@ -15,25 +15,34 @@ export class PreguntasUnirColorPage implements OnInit {
 
   @Input() numEjer: number;
   @Input() tipo: string;
+  @Input() ejercicios?: any[];
   public numPregunta = 1;
   public preguntaForm: FormGroup;
   public img_color = null;
   public img_asociada = null;
-  public preguntas : PreguntaUnir[] = [];
+  public preguntas : any[] = [];
   public crearError = false;
   public musica = '';
 
   @ViewChild('preguntaFormRef', { static: false }) preguntaFormRef: NgForm;
 
-  constructor(public audioService : AudioService, private modalCtr: ModalController, private photoService : CamaraService, private actionSheetCtrl: ActionSheetController, private navCtrl: NavController) { }
+  constructor(public audioService: AudioService, private modalCtr: ModalController, private photoService: CamaraService, private actionSheetCtrl: ActionSheetController, private navCtrl: NavController) { }
 
   ngOnInit() {
+    this.preguntas = this.ejercicios.map(ejercicio => ({
+      ...ejercicio,
+      formGroup: new FormGroup({
+        texto_1: new FormControl(ejercicio.texto_1, Validators.required),
+        texto_2: new FormControl(ejercicio.texto_2, Validators.required)
+      })
+    }));
+    console.log(this.numEjer);
     this.preguntaForm = new FormGroup({
       texto_color: new FormControl('', Validators.required),
       texto_img: new FormControl('', Validators.required)
     });
 
-    if(this.tipo == 'tutorial'){
+    if (this.tipo == 'tutorial') {
       this.numEjer = 0;
     }
   }
@@ -51,13 +60,18 @@ export class PreguntasUnirColorPage implements OnInit {
     await this.modalCtr.dismiss(closeModal);
   }
 
-  async add(){
-
+  async add() {
     this.preguntaFormRef.onSubmit(undefined);
 
-    if(this.preguntaForm.valid && this.img_color != null && this.img_asociada != null){
-
-      this.preguntas.push(new PreguntaUnir( null, this.img_color, this.preguntaForm.value.texto_color, this.img_asociada, this.preguntaForm.value.texto_img, this.musica, this.numEjer));
+    if (this.preguntaForm.valid && this.img_color != null && this.img_asociada != null) {
+      this.preguntas.push({
+        img_1: this.img_color,
+        texto_1: this.preguntaForm.value.texto_1,
+        img_2: this.img_asociada,
+        texto_2: this.preguntaForm.value.texto_2,
+        musica: this.musica,
+        numEjer: this.numEjer
+      });
       console.log(this.preguntas);
       this.numPregunta++;
 
@@ -67,83 +81,64 @@ export class PreguntasUnirColorPage implements OnInit {
     }
   }
 
-  async create(){
-    if(this.preguntas.length != 0){
+  async create() {
+    if (this.preguntas.length != 0) {
+      console.log(this.preguntas);
       await this.modalCtr.dismiss(this.preguntas);
-    }
-
-    else{
+    } else {
       this.crearError = true;
     }
-
   }
 
-  async delete(index : number){
+  async delete(index: number) {
     this.preguntas.splice(index, 1);
-    this.numPregunta --;
+    this.numPregunta--;
   }
 
-
-  async selectAudio(){
-
+  async selectAudio() {
     await this.audioService.seleccionarAudio().then(async (_) => {
       this.musica = this.audioService.returnpath;
-
     })
   }
 
-  getFoto(tipo: string) {
+  getFoto(tipo: string, pregunta?: any) {
     openGaleriaModal(this.modalCtr).then((data) => {
-      if(tipo == 'unirColor_color'){
+      if (tipo == 'unirColor_color') {
         this.img_color = data.img;
-      }
-
-      else if(tipo == 'unirColor_img_asociada'){
+      } else if (tipo == 'unirColor_img_asociada') {
         this.img_asociada = data.img;
-
+      }else if(tipo === 'img1'){
+        pregunta.img_1 = data.img;
+      }else if (tipo === 'img2'){
+        pregunta.img_2 = data.img;
       }
     });
-
   }
 
-
-  async presentActionSheet(dir : string) {
-
+  async presentActionSheet(dir: string) {
     const actionSheet = await this.actionSheetCtrl.create({
       header: 'Elige una opción',
       buttons: [{
         text: 'Galería',
         handler: async () => {
-
-          await  this.photoService.getGaleria().then(async (_) => {
-            if(dir == 'unirColor_color'){
+          await this.photoService.getGaleria().then(async (_) => {
+            if (dir == 'unirColor_color') {
               this.img_color = await this.photoService.imgURL;
-             }
-
-             else if(dir == 'unirColor_img_asociada'){
-               this.img_asociada = await this.photoService.imgURL;
-
-             }
+            } else if (dir == 'unirColor_img_asociada') {
+              this.img_asociada = await this.photoService.imgURL;
+            }
           });
         }
-
       }, {
         text: 'Cámara',
         handler: async () => {
-
-        await this.photoService.getCamara().then(async (_) => {
-          if(dir == 'unirColor_color'){
-           this.img_color = await this.photoService.imgURL;
-          }
-
-          else if(dir == 'unirColor_img_asociada'){
-            this.img_asociada = await this.photoService.imgURL;
-
-          }
-        });
-
-
-
+          await this.photoService.getCamara().then(async (_) => {
+            if (dir == 'unirColor_color') {
+              this.img_color = await this.photoService.imgURL;
+            } else if (dir == 'unirColor_img_asociada') {
+              this.img_asociada = await this.photoService.imgURL;
+            }
+          });
         }
       }, {
         text: 'Cancelar',
@@ -152,9 +147,6 @@ export class PreguntasUnirColorPage implements OnInit {
     });
 
     await actionSheet.present();
-
     const { role, data } = await actionSheet.onDidDismiss();
-
   }
-
 }
